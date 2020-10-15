e70e8e8793203861eb3a0990211aa869fbc26d77
krishna <bkesari@outlook.com>
2020-10-15 16:03:11 -0400

    Added PropTypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,16 @@
 
-import React, {Component} from 'react';
-import logo from '../Shared/images/logo.svg';
-
-//Importing Home component
+import React, { Component } from 'react';
 import Home from './Home/Home';
+import Header from "../Shared/components/layout/Header";
 import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                 {/* Adding Home Component */}
-                <Home />
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-            </div>
+    render(){
+        return(
+          <div className="App">
+              <Header title= "Learn" />
+              <Home />
+          </div>
         );
     }
 }
